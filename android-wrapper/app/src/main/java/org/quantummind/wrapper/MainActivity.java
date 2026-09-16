9a986b1e79a06353fbef75ba2f75526550213ca0
Emmanuel <emmanuel@softnet.local>
2026-09-16 18:18:34 +0100

    Add website wrapper APK

---
+++ android-wrapper/app/src/main/java/org/quantummind/wrapper/MainActivity.java
@@ -0,0 +1,20 @@
+package org.quantummind.wrapper;
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+public class MainActivity extends Activity {
+    private WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        webView = new WebView(this);
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://quantum-mind-kld3.onrender.com");
+        setContentView(webView);
+    }
+}
